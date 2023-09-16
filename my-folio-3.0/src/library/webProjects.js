8575029826827d10aae0c3bec6358d5ff7d0a495
mspaprika <arunepaprika@gmail.com>
2023-09-16 20:17:08 +0100

    deploying

---
+++ my-folio-3.0/src/library/webProjects.js
@@ -0,0 +1,25 @@
+/* 
+1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9
+10.
+*/
+
+export const webProjects = [
+    {
+        id: 1,
+        title: "My Lovely Cats",
+        description: "Cats is a family website created using my own cat pictures. It is a very special project for",
+        image: "https://i.imgur.com/2Z3QZ3M.png",
+        github: "https://github.com/mspaprika/Cats.git",
+        live: "https://mspaprika.github.io/Cats/",
+        lastUpdated: "10/03/2023",
+        mainFeatures: "HTML, CSS, JavaScript, Bootstrap.",
+    },
+]
